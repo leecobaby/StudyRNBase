9182ad80d0dc5d8e4f07f56720f75bdbe090f72f
Leeco <leeco19@qq.com>
2023-09-13 13:51:01 +0800

    s6: use fetch in react-native

--- js/pages/Detail.tsx
+++ js/pages/Detail.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button, TextInput, ScrollView} from 'react-native';
 
 import {ScreenProps} from '@/navigators/type';
 import {useAppDispatch} from '@/hooks/store';
@@ -8,18 +8,41 @@ import {toggleTheme} from '@/store/themeSlice';
 type Props = ScreenProps<'Detail'>;
 export const Detail: React.FC<Props> = ({navigation}) => {
   const dispatch = useAppDispatch();
+  const [value, setValue] = useState('');
+  const [data, setData] = useState([]);
+
+  const loadData = () => {
+    const url = `https://api.github.com/search/repositories?q=${value}`;
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(responseData => {
+        setData(responseData.items);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.text}>Detail Page</Text>
       <Button title="切换主题" onPress={() => dispatch(toggleTheme())} />
-    </View>
+
+      <TextInput style={styles.input} onChangeText={text => setValue(text)} value={value} />
+      <Button title="获取" onPress={loadData} />
+      <Text>{JSON.stringify(data)}</Text>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f5fcff',
@@ -29,4 +52,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  input: {height: 30, borderColor: 'gray', borderWidth: 1, width: 200},
 });
