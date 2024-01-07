8055dda7620395ea03b659ee598ab3916c3c71bc
Leeco <leeco19@qq.com>
2024-01-07 20:34:04 +0800

    s10: create about-base

--- js/GlobalStyles.ts
+++ js/GlobalStyles.ts
@@ -1,7 +1,5 @@
-import {StyleSheet} from 'react-native';
-
 const BackgroundColor = '#F5FCFF';
-export const GlobalStyles = StyleSheet.create({
+export const GlobalStyles = {
   line: {
     height: 0.5,
     opacity: 0.5,
@@ -11,4 +9,7 @@ export const GlobalStyles = StyleSheet.create({
     flex: 1,
     backgroundColor: BackgroundColor,
   },
-});
+  backgroundColor: BackgroundColor,
+  nav_bar_height_ios: 44,
+  nav_bar_height_android: 50,
+};
